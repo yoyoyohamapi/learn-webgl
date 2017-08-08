02acaf77ea591388aa7820bb31dd30f9f233cd85
吴晓军 <18581867753@163.com>
2017-08-08 22:05:54 +0800

    init

---
+++ DrawingRectangle.js
@@ -0,0 +1,7 @@
+function main() {
+	const canvas = document.getElementById('example');
+	const ctx = canvas.getContext('2d');
+
+	ctx.fillStyle = "rgba(0, 0, 255, 1.0)";
+	ctx.fillRect(120, 10, 150, 150);
+}
