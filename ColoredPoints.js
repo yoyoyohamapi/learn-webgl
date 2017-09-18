33e19298bf4327b16bb071187c52071d33e746a2
吴晓军 <18581867753@163.com>
2017-09-18 21:00:53 +0800

    Points

--- ColoredPoints.js
+++ ColoredPoints.js
@@ -10,9 +10,11 @@ function main() {
   `
 
   const FSHADER_SOURCE = `
+    precision mediump float;
+    uniform vec4 u_FragColor;
     void main() {
       // 颜色
-      gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+      gl_FragColor = u_FragColor;
     }
   `
 
@@ -31,12 +33,13 @@ function main() {
   }
 
   const a_Position = gl.getAttribLocation(gl.program, 'a_Position')
-
+  const u_FragColor = gl.getUniformLocation(gl.program, 'u_FragColor')
 
   gl.clearColor(0.0, 0.0, 0.0, 1.0)
   gl.clear(gl.COLOR_BUFFER_BIT)
 
   const points = []
+  const colors = []
   canvas.onmousedown = e => {
     const { clientX, clientY, target } = e
     const { height, width } = canvas
@@ -45,9 +48,37 @@ function main() {
     const y = -((clientY - top) - height/2)/(height/2)
     points.push({x, y})
 
+    if (x > 0.0 && y >= 0.0) {
+      // 第一象限
+      colors.push({
+        r: 1.0,
+        g: 0.0,
+        b: 0.0,
+        alpha: 1.0
+      })
+    } else if (x < 0.0 && y < 0.0) {
+      // 第三象限
+      colors.push({
+        r: 0.0,
+        g: 1.0,
+        b: 0.0,
+        alpha: 1.0
+      })
+    } else {
+      // 其他
+      colors.push({
+        r: 1.0,
+        g: 1.0,
+        b: 1.0,
+        alpha: 1.0
+      })
+    }
+
     gl.clear(gl.COLOR_BUFFER_BIT)
-    points.forEach(point => {
+    points.forEach((point, index) => {
+      const color = colors[index]
       gl.vertexAttrib3f(a_Position, point.x, point.y, 0, 0)
+      gl.uniform4f(u_FragColor, color.r, color.g, color.b, color.alpha)
       gl.drawArrays(gl.POINTS, 0, 1)
     })
   }
