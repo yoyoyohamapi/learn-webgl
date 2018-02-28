eafbf1b6c81ce9064e66b97194b7190dab1b6d56
surejinwu <surejinwu@tencent.com>
2018-02-28 15:33:44 +0800

    Cube

---
+++ ColoredCube.js
@@ -0,0 +1,85 @@
+function main() {
+  const canvas = document.querySelector('canvas')
+  const gl = canvas.getContext('webgl')
+  if (!gl) {
+    return
+  }
+
+  const program = createProgramFromScripts(gl, '#vertex-shader', '#fragment-shader')
+  gl.useProgram(program)
+
+  // 获得属性
+  const positionLocation = gl.getAttribLocation(program, 'a_position')
+  const colorLocation = gl.getAttribLocation(program, 'a_color')
+  // 获得全局变量
+  const matrixLocation = gl.getUniformLocation(program, 'u_matrix')
+
+  // 开启隐藏面消除
+  gl.enable(gl.DEPTH_TEST)
+
+  //  WebGL 中的立方体结构
+  //    v6----- v5
+  //   /|      /|
+  //  v1------v0|
+  //  | |     | |
+  //  | |v7---|-|v4
+  //  |/      |/
+  //  v2------v3
+
+  const vertices = new Float32Array([   // Vertex coordinates
+    1.0, 1.0, 1.0,  -1.0, 1.0, 1.0,  -1.0,-1.0, 1.0,   1.0,-1.0, 1.0,  // v0-v1-v2-v3 front
+    1.0, 1.0, 1.0,   1.0,-1.0, 1.0,   1.0,-1.0,-1.0,   1.0, 1.0,-1.0,  // v0-v3-v4-v5 right
+    1.0, 1.0, 1.0,   1.0, 1.0,-1.0,  -1.0, 1.0,-1.0,  -1.0, 1.0, 1.0,  // v0-v5-v6-v1 up
+   -1.0, 1.0, 1.0,  -1.0, 1.0,-1.0,  -1.0,-1.0,-1.0,  -1.0,-1.0, 1.0,  // v1-v6-v7-v2 left
+   -1.0,-1.0,-1.0,   1.0,-1.0,-1.0,   1.0,-1.0, 1.0,  -1.0,-1.0, 1.0,  // v7-v4-v3-v2 down
+    1.0,-1.0,-1.0,  -1.0,-1.0,-1.0,  -1.0, 1.0,-1.0,   1.0, 1.0,-1.0   // v4-v7-v6-v5 back
+ ])
+
+  const colors = new Float32Array([     // Colors
+    0.4, 0.4, 1.0,  0.4, 0.4, 1.0,  0.4, 0.4, 1.0,  0.4, 0.4, 1.0,  // v0-v1-v2-v3 front(blue)
+    0.4, 1.0, 0.4,  0.4, 1.0, 0.4,  0.4, 1.0, 0.4,  0.4, 1.0, 0.4,  // v0-v3-v4-v5 right(green)
+    1.0, 0.4, 0.4,  1.0, 0.4, 0.4,  1.0, 0.4, 0.4,  1.0, 0.4, 0.4,  // v0-v5-v6-v1 up(red)
+    1.0, 1.0, 0.4,  1.0, 1.0, 0.4,  1.0, 1.0, 0.4,  1.0, 1.0, 0.4,  // v1-v6-v7-v2 left
+    1.0, 1.0, 1.0,  1.0, 1.0, 1.0,  1.0, 1.0, 1.0,  1.0, 1.0, 1.0,  // v7-v4-v3-v2 down
+    0.4, 1.0, 1.0,  0.4, 1.0, 1.0,  0.4, 1.0, 1.0,  0.4, 1.0, 1.0   // v4-v7-v6-v5 back
+  ])
+
+  const indices = new Uint8Array([       // Indices of the vertices
+      0, 1, 2,   0, 3, 2,    // front
+      4, 5, 6,   4, 6, 7,    // right
+      8, 9,10,   8,10,11,    // up
+    12,13,14,  12,14,15,    // left
+    16,17,18,  16,18,19,    // down
+    20,21,22,  20,22,23     // back
+  ])
+
+  // 初始化索引 buffer
+  const indexBuffer = gl.createBuffer()
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer)
+  gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indices, gl.STATIC_DRAW)
+
+  // 设置位置和颜色
+  initArrayBuffer(gl, positionLocation, vertices, 3, gl.FLOAT)
+  initArrayBuffer(gl, colorLocation, colors, 3, gl.FLOAT)
+
+  // 设置视角和投影矩阵
+  const matrix = new Matrix4()
+  matrix.setPerspective(30, 1, 1, 100)
+  matrix.lookAt(3, 3, 7, 0, 0, 0, 0, 1, 0)
+  gl.uniformMatrix4fv(matrixLocation, false, matrix.elements)
+
+  // 清空画布
+  gl.clearColor(0,0,0,1)
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+
+  // 进行绘制
+  gl.drawElements(gl.TRIANGLES, indices.length, gl.UNSIGNED_BYTE, 0)
+}
+
+function initArrayBuffer(gl, location, data, num, type) {
+  const buffer = gl.createBuffer()
+  gl.bindBuffer(gl.ARRAY_BUFFER, buffer)
+  gl.bufferData(gl.ARRAY_BUFFER, data, gl.STATIC_DRAW)
+  gl.vertexAttribPointer(location, num, type, false, 0, 0)
+  gl.enableVertexAttribArray(location)
+}
